8773d36f46697dcb260b3d9f9971a7d32eb36179
hawkinsj1384 <31799868+hawkinsj1384@users.noreply.github.com>
2017-09-09 12:39:05 -0400

    Distance traveled over time

---
+++ M2HW1_DistanceTraveled_Jalessa Hawkins.py
@@ -0,0 +1,16 @@
+#CTI-110
+#M2HW1-Distance Traveled
+#Jalessa Hawkins
+#September 9, 2017
+
+ 
+# Calculate distance traveled at the speed of 70 miles per hour
+speed=70
+distanceAfter6= 6 * speed
+distanceAfter10= 10 * speed
+distanceAfter15= 15 * speed
+print('The total distance the car will travel after 6 hours is', (distanceAfter6), ('miles'))
+print('The total distance the car will travel after 10 hours is', (distanceAfter10), ('miles'))
+print('The total distance the car will travel after 15 hours is', format(distanceAfter15, ','), ('miles'))
+
+
