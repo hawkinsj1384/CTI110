05551661e0f3ebb20287a80da993fbfc6fa740a1
hawkinsj1384 <31799868+hawkinsj1384@users.noreply.github.com>
2017-09-09 10:14:38 -0400

    First Submission

---
+++ M2T1_SalesPrediction_Hawkins.py
@@ -0,0 +1,15 @@
+#CTI-110
+#M2T1-Sales Production
+#Jalessa Hawkins
+#September 6, 2017
+
+# Get the projected total sales
+total_sales= float(input('Enter the projected sales: '))
+
+#Calculat the profit as 23 percent of the total sales.
+profit = total_sales * 0.23
+
+# Display the profit.
+print('The profit is $', format(profit, ',.2f'))
+                                 
+
